81f1b69017fa52f183a59d658b9846c29751e1c7
BAM-hub <bsharamin12@gmail.com>
2022-04-04 00:59:43 +0300

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,91 @@
-import React from 'react';
-import { Logo, NavBarContainer, NavList } from './styles/NavBar.styled';
-import { UserOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { SecondaryText } from '../components/styles/Text.styled';
+import { 
+  Lable, 
+  Logo, 
+  NavBarContainer, 
+  NavList, 
+  ListTtem, 
+  Theme,
+  ThemeProvider,
+  ThemesContainer,
+  Toggle
+} from './styles/NavBar.styled';
+import { 
+  UserOutlined,
+  TableOutlined,
+  UnorderedListOutlined,
+  FundProjectionScreenOutlined,
+  BulbOutlined,
+} from '@ant-design/icons';
 
-const NavBar = ({ color }) => {
+const NavBar = ({ color, theme, setTheme }) => {
+  const [changeTheme, setChangeTheme] = useState(false);
+
   return (
     <NavBarContainer color={color}>
       <Logo>BAM</Logo>
       <NavList>
-        <li>
-          <UserOutlined />
-        </li>
+        <ListTtem>
+          <UserOutlined style={{fontSize: '1.5rem'}} />
+          <Lable>
+            <SecondaryText>Info</SecondaryText>
+          </Lable>
+        </ListTtem>
+
+        <ListTtem>
+          <TableOutlined style={{fontSize: '1.5rem'}} />
+          <Lable>
+            <SecondaryText>Time Line</SecondaryText>
+          </Lable>
+        </ListTtem>
+        <ListTtem>
+          <UnorderedListOutlined style={{fontSize: '1.5rem'}} />
+          <Lable>
+            <SecondaryText>Skills</SecondaryText>
+          </Lable>
+        </ListTtem>
+        <ListTtem>
+          <FundProjectionScreenOutlined style={{fontSize: '1.5rem'}} />
+          <Lable>
+            <SecondaryText>Projects</SecondaryText>
+          </Lable>
+        </ListTtem>
       </NavList>
+
+      <Theme>
+          <BulbOutlined 
+            onClick={() => setChangeTheme(true)}
+          />
+          {changeTheme && (
+            <ThemeProvider>
+              <ThemesContainer>
+                <p>Dark</p>
+                <Toggle 
+                  theme={theme} 
+                  toggleName='dark' 
+                  color={color}
+                  onClick={() => {
+                    setTheme('dark');
+                    setTimeout(() => {setChangeTheme(false)}, 200);
+                  }}
+                />
+              </ThemesContainer>
+              <ThemesContainer>
+                <p>Light</p>
+                <Toggle 
+                  theme={theme} 
+                  toggleName='light'
+                  color={color} 
+                  onClick={() => {
+                    setTheme('light');
+                    setTimeout(() => {setChangeTheme(false)}, 200);
+                  }}
+                />
+              </ThemesContainer>
+            </ThemeProvider>
+          )}
+      </Theme>
     </NavBarContainer>
   );
 };
