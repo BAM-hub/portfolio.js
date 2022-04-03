65a06e3d10a510604389b220318c6849abad9f04
BAM-hub <bsharamin12@gmail.com>
2022-04-04 01:01:27 +0300

    my info styled components

---
+++ src/components/styles/MyInfo.styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const MyInfoContainer = styled.div`
+  height: 94vh;
+  /* width: 70vw; */
+  display: flex;
+  padding: 1rem;
+  /* overflow: hidden; */
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: .2rem;
+`;
+
+export const SVGContainer = styled.img`
+  width: 40vw;
+  /* height: 94vh; */
+`;
