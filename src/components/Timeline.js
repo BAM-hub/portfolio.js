cd0241ea5c161bfdd5e1d55723ba909ee1799a83
BAM-hub <bsharamin12@gmail.com>
2022-04-05 04:54:15 +0300

    added the ligic for the card and did some changes to the markup

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { PrimaryText, SecondaryText } from "./styles/Text.styled";
+import { 
+  PrimaryText,
+  MotionPrimaryText,
+  MotionSecondaryText,
+  AccentPointer,
+  SecondaryText
+} from "./styles/Text.styled";
 import { TIMELINE_DATA } from './timelineData';
 import { motion } from "framer-motion";
 import { 
@@ -7,45 +13,85 @@ import {
   MotionTitle, 
   TimelineContainer, 
   YearInfoContainer,
-  Date
+  EmptyYearInfoContainer,
+  Date,
+  SkillTitle,
+  ShowLessButton
 } from "./styles/Timeline.styled";
 import { FileUnknownOutlined } from "@ant-design/icons";
 
-const Timeline = () => {
+const Timeline = ({ color, theme }) => {
   const [selectedData, setData] = useState(TIMELINE_DATA[1]);
   const [isOpen, setIsOpen] = useState(false);
+  const [skill, setSkill] = useState(0);
 
   return (
     <TimelineContainer>
       <DateContainer>
         {
           TIMELINE_DATA.map((data, i) => (
-            <Date 
+            <Date
               key={i}
               selected={data.year === selectedData.year}
               onClick={() => setData(TIMELINE_DATA[i])}
               >
-              <PrimaryText>{data.year}</PrimaryText> 
+              <PrimaryText
+                theme={theme}
+              >{data.year}</PrimaryText> 
             </Date>
           ))
         }
       </DateContainer>
       {selectedData.data === 'none' ? (
-        <>
+        <EmptyYearInfoContainer theme={theme}>
           <PrimaryText>Nothing to Show.</PrimaryText>
           <FileUnknownOutlined />
-        </>
+        </EmptyYearInfoContainer>
       ) : 
-      <YearInfoContainer>
+      <YearInfoContainer theme={theme} >
         <MotionTitle 
+          theme={theme}
           as={motion.div}
-          onClick={() => setIsOpen(!isOpen)}
           layout
+          transition={{layout:{duration: .3, type: "spring"}}}
         >
-          <motion.p layout>{selectedData.data.projects[0].skillName}</motion.p>
+          {
+            selectedData.data.projects.map((project, i) =>(
+              <SkillTitle 
+                key={i}
+                onClick={()=> {
+                  setSkill(i);
+                  setIsOpen(true);
+                }}
+              >
+                <AccentPointer color={color} />
+                <motion.p layout >
+                  {project.skillName}
+                </motion.p>
+            </SkillTitle>
+            ))
+          }
+
           {isOpen && <>
-            <PrimaryText>{selectedData.data.projects[0].description}</PrimaryText>
-            <SecondaryText>{selectedData.data.skills}</SecondaryText>
+            <ShowLessButton onClick={() => setIsOpen(false)} theme={theme}>
+              <SecondaryText>Show less</SecondaryText>
+            </ShowLessButton>
+            <MotionSecondaryText 
+              as={motion.p}
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: .7 }}
+              transition={{ duration: .2 }}
+              theme={theme}
+              >{selectedData.data.projects[skill].description}</MotionSecondaryText>
+            <MotionPrimaryText
+              as={motion.p}
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: .2 }}
+              theme={theme}
+            >{selectedData.data.skills.join(' | ')}</MotionPrimaryText>
           </>
           }
         </MotionTitle>
