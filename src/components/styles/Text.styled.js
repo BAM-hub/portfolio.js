cb3d994e4b254b9fc30d0ef870fe3a5b5797ef8d
BAM-hub <bsharamin12@gmail.com>
2022-06-24 00:14:22 +0300

    passing props to text for reactivity

--- src/components/styles/Text.styled.js
+++ src/components/styles/Text.styled.js
@@ -1,31 +1,33 @@
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-export const SecondaryText = styled.p`
-  color: rgba(0, 0, 0, 0.66);
-`;
-
-export const PrimaryText = styled.p.attrs(props => ({
+export const SecondaryText = styled.p.attrs((props) => ({
   theme: props.theme,
 }))`
-  color: ${props => props.theme === 'dark' ? 'white': 'black'};
+  color: ${(props) => (props.theme === "dark" ? "white" : "black")};
+  opacity: 0.66;
 `;
 
-export const MotionPrimaryText = styled(motion.p).attrs(props => ({
-  theme: props.theme
+export const PrimaryText = styled.p.attrs((props) => ({
+  theme: props.theme,
 }))`
-  color: ${props => props.theme === 'dark' 
-  ? 'black': 'white'};
+  color: ${(props) => (props.theme === "dark" ? "white" : "black")};
+`;
+
+export const MotionPrimaryText = styled(motion.p).attrs((props) => ({
+  theme: props.theme,
+}))`
+  color: ${(props) => (props.theme === "dark" ? "black" : "white")};
 `;
 
 export const MotionSecondaryText = styled(motion.p)`
   font-weight: 200;
 `;
 
-export const AccentPointer = styled.div.attrs(props => ({
-  color: props.color|| 'black'
+export const AccentPointer = styled.div.attrs((props) => ({
+  color: props.color || "black",
 }))`
-  background: ${props => props.color};
-  width: .25rem;
+  background: ${(props) => props.color};
+  width: 0.25rem;
   height: 1rem;
 `;
