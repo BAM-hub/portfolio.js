31d08ac905bb71e7c33d8e351ceb999d25a1544c
BAM-hub <bsharamin12@gmail.com>
2022-06-24 01:16:17 +0300

    minor tweaks

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,24 +1,22 @@
 import { useState } from "react";
-import { 
+import {
   PrimaryText,
   MotionPrimaryText,
   MotionSecondaryText,
   AccentPointer,
-  SecondaryText
+  SecondaryText,
 } from "./styles/Text.styled";
-import { TIMELINE_DATA } from './timelineData';
+import { TIMELINE_DATA } from "./timelineData";
 import { motion } from "framer-motion";
-import { 
-  DateContainer, 
-  MotionTitle, 
-  TimelineContainer, 
+import {
+  DateContainer,
+  MotionTitle,
+  TimelineContainer,
   YearInfoContainer,
-  EmptyYearInfoContainer,
   Date,
   SkillTitle,
-  ShowLessButton
+  ShowLessButton,
 } from "./styles/Timeline.styled";
-import { FileUnknownOutlined } from "@ant-design/icons";
 
 const Timeline = ({ color, theme }) => {
   const [selectedData, setData] = useState(TIMELINE_DATA[1]);
@@ -28,76 +26,65 @@ const Timeline = ({ color, theme }) => {
   return (
     <TimelineContainer>
       <DateContainer>
-        {
-          TIMELINE_DATA.map((data, i) => (
-            <Date
-              key={i}
-              selected={data.year === selectedData.year}
-              onClick={() => setData(TIMELINE_DATA[i])}
-              >
-              <PrimaryText
-                theme={theme}
-              >{data.year}</PrimaryText> 
-            </Date>
-          ))
-        }
+        {TIMELINE_DATA.map((data, i) => (
+          <Date
+            key={i}
+            selected={data.year === selectedData.year}
+            onClick={() => setData(TIMELINE_DATA[i])}
+          >
+            <PrimaryText theme={theme}>{data.year}</PrimaryText>
+          </Date>
+        ))}
       </DateContainer>
-      {selectedData.data === 'none' ? (
-        <EmptyYearInfoContainer theme={theme}>
-          <PrimaryText>Nothing to Show.</PrimaryText>
-          <FileUnknownOutlined />
-        </EmptyYearInfoContainer>
-      ) : 
-      <YearInfoContainer theme={theme} >
-        <MotionTitle 
+      <YearInfoContainer theme={theme}>
+        <MotionTitle
           theme={theme}
           as={motion.div}
           layout
-          transition={{layout:{duration: .3, type: "spring"}}}
+          transition={{ layout: { duration: 0.3, type: "spring" } }}
         >
-          {
-            selectedData.data.projects.map((project, i) =>(
-              <SkillTitle 
-                key={i}
-                onClick={()=> {
-                  setSkill(i);
-                  setIsOpen(true);
-                }}
-              >
-                <AccentPointer color={color} />
-                <motion.p layout >
-                  {project.skillName}
-                </motion.p>
+          {selectedData.data.projects.map((project, i) => (
+            <SkillTitle
+              key={i}
+              onClick={() => {
+                setSkill(i);
+                setIsOpen(true);
+              }}
+            >
+              <AccentPointer color={color} />
+              <motion.p layout>{project.skillName}</motion.p>
             </SkillTitle>
-            ))
-          }
+          ))}
 
-          {isOpen && <>
-            <ShowLessButton onClick={() => setIsOpen(false)} theme={theme}>
-              <SecondaryText>Show less</SecondaryText>
-            </ShowLessButton>
-            <MotionSecondaryText 
-              as={motion.p}
-              layout
-              initial={{ opacity: 0 }}
-              animate={{ opacity: .7 }}
-              transition={{ duration: .2 }}
-              theme={theme}
-              >{selectedData.data.projects[skill]?.description}</MotionSecondaryText>
-            <MotionPrimaryText
-              as={motion.p}
-              layout
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: .2 }}
-              theme={theme}
-            >{selectedData.data.skills.join(' | ')}</MotionPrimaryText>
-          </>
-          }
+          {isOpen && (
+            <>
+              <ShowLessButton onClick={() => setIsOpen(false)} theme={theme}>
+                <SecondaryText>Show less</SecondaryText>
+              </ShowLessButton>
+              <MotionSecondaryText
+                as={motion.p}
+                layout
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 0.7 }}
+                transition={{ duration: 0.2 }}
+                theme={theme}
+              >
+                {selectedData.data.projects[skill]?.description}
+              </MotionSecondaryText>
+              <MotionPrimaryText
+                as={motion.p}
+                layout
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.2 }}
+                theme={theme}
+              >
+                {selectedData.data.skills.join(" | ")}
+              </MotionPrimaryText>
+            </>
+          )}
         </MotionTitle>
       </YearInfoContainer>
-      }
-
     </TimelineContainer>
   );
 };
--- src/components/styles/Timeline.styled.js
+++ src/components/styles/Timeline.styled.js
@@ -1,68 +1,48 @@
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export const TimelineContainer = styled.div`
   height: 96vh;
   display: flex;
   flex-direction: column;
-  justify-content: space-around;
+  /* justify-content: space-around; */
+  gap: 2rem;
   padding: 1rem;
 `;
 
-export const DateContainer = styled.div.attrs(props => ({
-  theme: props.theme
+export const DateContainer = styled.div.attrs((props) => ({
+  theme: props.theme,
 }))`
   display: flex;
   width: 1fr;
   justify-content: space-around;
 `;
 
-export const Date = styled.div.attrs(props => ({
-    selected: props.selected,
+export const Date = styled.div.attrs((props) => ({
+  selected: props.selected,
 }))`
   width: calc(1rem * 3);
   border-bottom: 2px solid black;
-  border-color: ${props => props.selected ? 'white' : 'black'};
+  border-color: ${(props) => (props.selected ? "white" : "black")};
   :hover {
     cursor: pointer;
   }
 `;
 
-export const YearInfoContainer = styled.div.attrs(props => ({
+export const YearInfoContainer = styled.div.attrs((props) => ({
   theme: props.theme,
 }))`
   height: 60vh;
-  border: 1px solid ${props => props.theme === 'dark' ? 'white' : 'black'};
+  border: 1px solid ${(props) => (props.theme === "dark" ? "white" : "black")};
   width: 80%;
   margin: 0 auto;
   padding: 1rem;
 `;
 
-export const EmptyYearInfoContainer = styled.div.attrs(props => ({
+export const MotionTitle = styled(motion.div).attrs((props) => ({
   theme: props.theme,
 }))`
-  height: 60vh;
-  border: 1px solid ${props => props.theme === 'dark' ? 'white' : 'black'};
-  width: 80%;
-  margin: 0 auto;
-  padding: 1rem;
-  background-color: ${props => props.theme === 'dark' ? 'white' : 'black'};
-  font-size: 2rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-  opacity: 0.99;
-  > * {
-    color:  ${props => props.theme === 'dark' ? 'black' : 'white'};
-  }
-`;
-
-
-export const MotionTitle = styled(motion.div).attrs(props => ({
-  theme: props.theme
-}))`
-  background: ${props => props.theme === 'dark' ? 'white' : 'black'};
+  background: ${(props) => (props.theme === "dark" ? "white" : "black")};
   width: 100%;
   padding: 1rem 3rem;
   overflow: hidden;
@@ -72,17 +52,20 @@ export const MotionTitle = styled(motion.div).attrs(props => ({
   }
   > * {
     line-height: 120%;
-    color: ${props => props.theme === 'dark' ? 'black' : 'white'};
+    color: ${(props) => (props.theme === "dark" ? "black" : "white")};
   }
-`; 
+`;
 
 export const SkillTitle = styled(motion.div)`
   display: flex;
   gap: 1rem;
+  :hover {
+    opacity: 0.8;
+  }
 `;
 
-export const ShowLessButton = styled.div.attrs(props => ({
-  theme: props.theme
+export const ShowLessButton = styled.div.attrs((props) => ({
+  theme: props.theme,
 }))`
   width: fit-content;
   height: 2rem;
@@ -90,7 +73,6 @@ export const ShowLessButton = styled.div.attrs(props => ({
   top: 1rem;
   right: 1rem;
   > * {
-    color: ${props => props.theme === 'dark' 
-    ? '#5A5A5A': '#929292'};
+    color: ${(props) => (props.theme === "dark" ? "#5A5A5A" : "#929292")};
   }
 `;
