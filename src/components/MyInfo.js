4742873ca28619dd999da65827add78628c95e9e
BAM-hub <bsharamin12@gmail.com>
2022-06-24 00:13:43 +0300

    removed the svg and redesigend th text

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import { PrimaryText, SecondaryText } from './styles/Text.styled';
-import persona from '../assets/pair-programming 1.svg';
-import { 
+import React from "react";
+import { PrimaryText, SecondaryText } from "./styles/Text.styled";
+// import persona from "../assets/pair-programming 1.svg";
+import {
   MyInfoContainer,
-  SVGContainer, 
-  TextContainer
-} from './styles/MyInfo.styled';
+  // SVGContainer,
+  TextContainer,
+} from "./styles/MyInfo.styled";
 
-const MyInfo = () => {
+const MyInfo = ({ theme }) => {
   return (
     <MyInfoContainer>
       <TextContainer>
-        <h2>
-            <PrimaryText>
-                I'm Bshar Amin welcome to my protfolio.
-            </PrimaryText>
-        </h2>
         <h4>
-            <SecondaryText>
-            I’m a student at al-balqa applied university, I study CIS(Computer Information Systems).
-            I live in Amman, Jordan, Marka.
-            </SecondaryText>
+          <SecondaryText theme={theme}>
+            I'm Bshar Amin welcome to my protfolio.
+          </SecondaryText>
         </h4>
       </TextContainer>
-      <SVGContainer src={persona} alt="persona" />
+      <TextContainer>
+        <h3>
+          <PrimaryText theme={theme}>
+            I’m a student at al-balqa applied university, I study CIS(Computer
+            Information Systems). I live in Amman, Jordan, Marka.
+          </PrimaryText>
+        </h3>
+      </TextContainer>
+      {/* <SVGContainer src={persona} alt="persona" /> */}
     </MyInfoContainer>
   );
 };
