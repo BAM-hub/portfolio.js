1adeb92c4ebe04b0f8b8c0495ed7ae99927a114b
BAM-hub <bsharamin12@gmail.com>
2022-06-24 00:13:03 +0300

    comment out components to focus on current tasks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 import About from "./components/About";
-import NavBar from './components/NavBar';
-
+import MyInfo from "./components/MyInfo";
+import NavBar from "./components/NavBar";
+import Timeline from "./components/Timeline";
+import { Layout } from "./components/styles/Layout.styled";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 const App = () => {
-  const [color, setColor] = useState('#55EFC4');
+  const [color, setColor] = useState("#55EFC4");
+  const [theme, setTheme] = useState("light");
 
   return (
-    <div>
-      <NavBar 
-        color={color}
-      />
-      <About 
-        color={color}
-        setColor={setColor}
-      />
-    </div>
+    <Layout theme={theme}>
+      <NavBar color={color} theme={theme} setTheme={setTheme} />
+      <About color={color} setColor={setColor} />
+      <MyInfo theme={theme} />
+      <Timeline color={color} theme={theme} />
+      {/* <Skills color={color} theme={theme} /> */}
+      {/* <Projects color={color} theme={theme} /> */}
+    </Layout>
   );
-}
+};
 
 export default App;
